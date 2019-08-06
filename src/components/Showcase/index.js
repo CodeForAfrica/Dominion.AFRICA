cd12048a2db49459836e7f9083719e07e198e67c
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-08-06 16:16:23 +0300

    fix alignment

--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -8,9 +8,13 @@ import { withStyles } from '@material-ui/core/styles';
 import StoryList from './StoryList';
 
 const styles = theme => ({
+  showCaseContainer: {
+    backgroundColor: 'white',
+    display: 'inline-block',
+    width: '100%'
+  },
   root: {
     flexGrow: 1,
-    backgroundColor: 'white',
     padding: '3.1875rem 1.875rem',
     [theme.breakpoints.up('md')]: {
       padding: '3.1875rem 0',
@@ -45,38 +49,40 @@ function Showcase({ classes, showcaseStories, dominion: { selectedCountry } }) {
     );
   }
   return (
-    <Grid
-      container
-      direction="column"
-      className={classes.root}
-      justify="center"
-    >
+    <div className={classes.showCaseContainer}>
       <Grid
-        item
-        xs={12}
         container
-        direction="row"
-        className={classes.headline}
-        justify="flex-start"
-        alignItems="center"
+        direction="column"
+        className={classes.root}
+        justify="center"
       >
-        <Grid item xs={12}>
-          <Typography variant="h2" className={classes.headlineTitle}>
-            Showcase
-          </Typography>
-        </Grid>
+        <Grid
+          item
+          xs={12}
+          container
+          direction="row"
+          className={classes.headline}
+          justify="flex-start"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <Typography variant="h2" className={classes.headlineTitle}>
+              Showcase
+            </Typography>
+          </Grid>
 
+          <Grid item xs={12}>
+            <Typography variant="body2" className={classes.headlineDescription}>
+              Explore how land and how it is controlled shapes everything from
+              our food security and geopolitics to national identity.
+            </Typography>
+          </Grid>
+        </Grid>
         <Grid item xs={12}>
-          <Typography variant="body2" className={classes.headlineDescription}>
-            Explore how land and how it is controlled shapes everything from our
-            food security and geopolitics to national identity.
-          </Typography>
+          <StoryList storyData={stories} />
         </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <StoryList storyData={stories} />
-      </Grid>
-    </Grid>
+    </div>
   );
 }
 
