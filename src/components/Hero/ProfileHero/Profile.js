174e365e962820edb5dc656706f019605c326b39
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-08-14 14:44:01 +0300

    weight

--- src/components/Hero/ProfileHero/Profile.js
+++ src/components/Hero/ProfileHero/Profile.js
@@ -237,7 +237,6 @@ function Profile({ classes, dominion, geoId, history, ...props }) {
                 geoLayerFocusStyle={{
                   fillColor: 'rgb(127, 148, 66)',
                   color: 'rgb(141, 141, 140)',
-                  weight: 2,
                   opacity: 1,
                   fillOpacity: 1
                 }}
