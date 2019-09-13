25153d4466da4d4681ac3248c32e3672eec02896
nerdlet <brndnyokabi@gmail.com>
2019-09-13 16:01:49 +0300

    Add modal open on contact us link

--- src/pages/ServerError.js
+++ src/pages/ServerError.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
 import Page from '../components/Page';
-
 import config from '../config';
 import { TitlePageHeader } from '../components/Header';
 import ErrorContent from '../components/ErrorContent';
 
+import useToggleModal from '../useToggleModal';
+
+const useStyles = makeStyles({
+  link: {
+    textDecoration: 'underline'
+  }
+});
+
 function ServerError() {
+  const classes = useStyles();
+  const { toggleModal: toggleContact } = useToggleModal('contact');
+
   return (
     <Page>
       <TitlePageHeader dominion={config} profile={{}} />
@@ -14,7 +25,15 @@ function ServerError() {
         description={[
           'We are having some trouble processing your request.',
           'We have logged the error and will investigate. You can try again later or if the issues persisit, please ',
-          <a href="#contact">contact us.</a>
+          <span
+            role="link"
+            tabIndex={0}
+            onClick={toggleContact}
+            className={classes.link}
+            onKeyDown={toggleContact}
+          >
+            contact us.
+          </span>
         ]}
       />
     </Page>
