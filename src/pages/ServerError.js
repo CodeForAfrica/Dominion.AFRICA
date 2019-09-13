e38db6d749bb74934ec5486119a31543004e9649
nerdlet <brndnyokabi@gmail.com>
2019-09-13 15:15:36 +0300

    Add contact us link

--- src/pages/ServerError.js
+++ src/pages/ServerError.js
@@ -11,9 +11,11 @@ function ServerError() {
       <TitlePageHeader dominion={config} profile={{}} />
       <ErrorContent
         title="500 - Internal Server Error"
-        description="We are having some trouble processing your request.
-              We have logged the error and will investigate. You can try again later or if the issues persisit,
-              please contact us"
+        description={[
+          'We are having some trouble processing your request.',
+          'We have logged the error and will investigate. You can try again later or if the issues persisit, please ',
+          <a href="#contact">contact us.</a>
+        ]}
       />
     </Page>
   );
