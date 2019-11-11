8b7872ed68a7d0a63af6053d7866bbaad5915279
Clemence Kyara <clemence@codeforafrica.org>
2019-11-11 17:57:18 +0300

    Excluse server-side Tabletop dependencies on client

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 
-module.exports = withCSS(withImages());
+module.exports = withCSS(
+  withImages({
+    webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
+      // Note: we provide webpack above so you should not `require` it
+      // Perform customizations to webpack config
+      // Important: return the modified config
+
+      // https://github.com/jsoma/tabletop/issues/158
+      if (!isServer) {
+        // eslint-disable-next-line no-param-reassign
+        config.externals = ['tls', 'net', 'fs'];
+      }
+      return config;
+    }
+  })
+);
