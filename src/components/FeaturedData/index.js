fadcd17ec1086dfdc7e50071fd5b3dbfe6c37884
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-01-06 09:40:19 +0300

    initial page

--- src/components/FeaturedData/index.js
+++ src/components/FeaturedData/index.js
@@ -6,9 +6,11 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 const useStyles = makeStyles(({ palette, breakpoints }) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: palette.primary.light
+  },
+  layout: {
     padding: '60px 0',
     margin: '0 auto',
-    backgroundColor: palette.primary.light,
     [breakpoints.up('sm')]: {
       maxWidth: '66.59rem' // .75 of lg
     },
@@ -28,16 +30,18 @@ function FeaturedData() {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      direction="row"
-      justify="space-between"
-      alignItems="flex-start"
-      className={classes.root}
-    >
-      <Grid item />
-      <Grid item />
-    </Grid>
+    <div className={classes.root}>
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="flex-start"
+        className={classes.layout}
+      >
+        <Grid item />
+        <Grid item />
+      </Grid>
+    </div>
   );
 }
 
