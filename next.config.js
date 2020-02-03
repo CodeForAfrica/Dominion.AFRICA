5edf691c1e76ecfaea6f4113a069aae78e460a8e
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-02-03 09:51:13 +0300

    Add bundle analyzer + code reduction

--- next.config.js
+++ next.config.js
@@ -1,21 +1,26 @@
 /** This file runs under node and hence shouldn't contain
  * any `processable` syntax
  */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 
-module.exports = withCSS(
-  withImages({
-    inlineImageLimit: 300000,
-    webpack(config, { isServer }) {
-      // Important: return the modified config
+module.exports = withBundleAnalyzer(
+  withCSS(
+    withImages({
+      inlineImageLimit: 50000,
+      webpack(config, { isServer }) {
+        // Important: return the modified config
 
-      // https://github.com/jsoma/tabletop/issues/158
-      if (!isServer) {
-        // eslint-disable-next-line no-param-reassign
-        config.externals = ['tls', 'net', 'fs'];
+        // https://github.com/jsoma/tabletop/issues/158
+        if (!isServer) {
+          // eslint-disable-next-line no-param-reassign
+          config.externals = ['tls', 'net', 'fs'];
+        }
+        return config;
       }
-      return config;
-    }
-  })
+    })
+  )
 );
